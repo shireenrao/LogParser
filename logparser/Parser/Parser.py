9d4f9d9b408ecea816a1c3a84ed77582475f2670
shireenrao <shireenrao@gmail.com>
2013-07-15 07:36:45 -0400

    Implement Parser & actions for all events

--- logparser/Parser/Parser.py
+++ logparser/Parser/Parser.py
@@ -1,4 +1,70 @@
+import re
+import sys
+
+emptyField = '-'
+
+# Ordinals in the array from each log's entry
+
+ipCol = 9
+userAgentCol = 10
+urlReqCol = 5
+urlParamsCol = 6
+dateCol = 0
+timeCol = 1
 
 class Parser:
     def __init__(self, filename):
         self.filename = filename
+
+    def parseLogFile(self,  pattern):
+        self.cleanStuff()
+        lines = []
+        try:
+            if pattern.length() == 0:
+                #let's catch every line that does'nt start with #
+                pattern = '^[^#]'
+            input = open(self.fileName)
+            lines = input.readlines()
+            input.close()
+ 
+            for line in lines:
+                match = re.search(str(pattern),  line,  re.I)
+                if match:
+                    tokens = line.split(' ')
+                    ip = tokens[ipCol]
+                    userAgent = tokens[userAgentCol]
+                    urlReq = tokens[urlReqCol]
+                    if tokens[urlParamsCol] != emptyField:
+                        urlReq+='?'+tokens[urlParamsCol]
+                    dateTime = tokens[dateCol] + ' ' + tokens[timeCol]
+                    logEntry = W3SVCLogEntry(ip, userAgent, urlReq, dateTime)
+                    
+                    if not self.parsedRecords.has_key(ip):
+                        self.parsedRecords[ip] = {}
+                        
+                    if not self.parsedRecords[ip].has_key(userAgent):
+                        self.parsedRecords[ip][userAgent] = []
+                    
+                    self.parsedRecords[ip][userAgent].append(logEntry)
+        except IOError,  exc:
+            self.lastError = 'IOError: ' + str(exc)       
+        except IndexError,  exc:
+            self.lastError = 'IndexError: ' + str(exc)
+        except:
+            if self.lastError == '':
+                self.lastError = 'Unexpected error: ' + str(sys.exc_info()[1])
+                print "Unexpected error:", sys.exc_info()[0]
+            pass
+         
+
+    def cleanStuff(self):
+        self.parsedRecords = {}
+        self.lastError = ''
+        
+
+class W3SVCLogEntry:
+    def __init__(self,  ip,  userAgent,  urlReq,  dateTime):
+        self.ip = ip
+        self.userAgent = userAgent
+        self.urlReq = urlReq
+        self.dateTime = dateTime
--- logparser/ui/MainWindow.py
+++ logparser/ui/MainWindow.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import Parser
 
 """
 Module implementing MainWindow.
@@ -25,29 +26,47 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         """
         Slot documentation goes here.
         """
-        # TODO: not implemented yet
-        raise NotImplementedError
+        self.tvResult.clear()
+        fName = self.lblpath.text()
+        pattern = self.txtReToMatch.displayText()
+        parser = Parser.Parser(fName)
+        parser.parseLogFile(pattern)
+        
+        if 0<len(parser.lastError):
+            self.lblPath.setText(parser.lastError)
+        else:
+            for ip_k in parser.parsedRecords.keys():
+                item = QTreeWidgetItem(self.tvResult)
+                item.setText(0,  ip_k)
+                self.tvResult.addTopLevelItem(item)
+                for ua_k in parser.parsedRecords[ip_k].keys():
+                    item2 = QTreeWidgetItem(item)
+                    item2.setText(0,  ua_k)
+                    item.addChild(item2)
+                    for logEntry in parser.parsedRecords[ip_k][ua_k]:
+                        item3 = QTreeWidgetItem(item2)
+                        item3.setText(0,  logEntry.urlReq)
+                        item3.setText(1,  logEntry.dateTime)
+                        item2.addChild(item3)
     
     @pyqtSignature("")
     def on_actionOpen_log_file_triggered(self):
         """
         Slot documentation goes here.
         """
-        # TODO: not implemented yet
-        raise NotImplementedError
+        fName = QFileDialog.getOpenFileName(None, self.trUtf8("Select a W3SVC log file to parse"), QString(), self.trUtf8("*.log"), None)
+        self.lblPath.setText(fName)
     
     @pyqtSignature("")
     def on_actionQuit_triggered(self):
         """
         Slot documentation goes here.
         """
-        # TODO: not implemented yet
-        raise NotImplementedError
+        self.close()
     
     @pyqtSignature("")
     def on_actionAbout_triggered(self):
         """
         Slot documentation goes here.
         """
-        # TODO: not implemented yet
-        raise NotImplementedError
+        QMessageBox.information(self, "About", "IIS log files parser")
